8a45b60e92528fda88f549b2e2c2adfae564d6d4
a2-b2 <101610797+a2-b2@users.noreply.github.com>
2023-03-07 16:32:20 -0700

    Updated RoutineStartScreen
    Created ExerciseScreen. Finished most of the skeleton for RoutineStartScreen.

--- CPSC481.FinalProject/RoutineStartScreen.xaml.cs
+++ CPSC481.FinalProject/RoutineStartScreen.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace CPSC481.FinalProject
@@ -17,19 +18,23 @@ namespace CPSC481.FinalProject
     /// <summary>
     /// Interaction logic for RoutineStartScreen.xaml
     /// </summary>
-    public partial class RoutineStartScreen : Window
+    public partial class RoutineStartScreen : Page
     {
         int num_of_exercises = 1;
         public RoutineStartScreen()
         {
             InitializeComponent();
-
             List<ExerciseItem> exercises = new List<ExerciseItem>();
             exercises.Add(new ExerciseItem() { Num = num_of_exercises++, Name = "Knee-To-Chest"});
             exercises.Add(new ExerciseItem() { Num = num_of_exercises++, Name = "Seated Spinal Twist" });
             exercises.Add(new ExerciseItem() { Num = num_of_exercises++, Name = "Cat Stretch" });
 
             icRoutineList.ItemsSource = exercises;
+        }
+
+        private void StartRoutine(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 
---
+++ CPSC481.FinalProject/TestWindow1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Navigation;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CPSC481.FinalProject
+{
+    /// <summary>
+    /// Interaction logic for TestWindow1.xaml
+    /// </summary>
+    public partial class TestWindow1 : Window
+    {
+        public TestWindow1()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenRoutineStartScreen(object sender, RoutedEventArgs e)
+        {
+            RoutineStartScreen routineStartScreen = new RoutineStartScreen();
+            
+            this.Content = routineStartScreen;
+        }
+    }
+}
