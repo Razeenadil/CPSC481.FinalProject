c86ae7eb29892cfd6d194f46d2f282c3853ee221
Muhammad Razeen Adil <razeen.adil16@gmail.com>
2023-03-06 17:00:08 -0700

    created the radial navigation button

--- CPSC481.FinalProject/MainWindow.xaml.cs
+++ CPSC481.FinalProject/MainWindow.xaml.cs
@@ -20,9 +20,32 @@ namespace CPSC481.FinalProject
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool NavigationIsClicked;
         public MainWindow()
         {
             InitializeComponent();
+            NavigationIsClicked = false;
+        }
+
+        private void NavigationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(! NavigationIsClicked)
+            {
+                InfoButton.Visibility = Visibility.Visible;
+                DemoButton.Visibility = Visibility.Visible;
+                ProgressButton.Visibility = Visibility.Visible;
+                RoutineButton.Visibility = Visibility.Visible;
+                NavigationIsClicked = true;
+            }
+            else
+            {
+                InfoButton.Visibility = Visibility.Hidden;
+                DemoButton.Visibility = Visibility.Hidden;
+                ProgressButton.Visibility = Visibility.Hidden;
+                RoutineButton.Visibility = Visibility.Hidden;
+                NavigationIsClicked = false;
+            }
+        
         }
     }
 }
