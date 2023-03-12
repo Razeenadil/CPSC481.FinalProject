42e8775b11616be22d774fd3bfbf8f3209fe68f9
Muhammad Razeen Adil <razeen.adil16@gmail.com>
2023-03-12 17:39:18 -0600

    landing page and new icon for information

--- CPSC481.FinalProject/MainWindow.xaml.cs
+++ CPSC481.FinalProject/MainWindow.xaml.cs
@@ -337,7 +337,7 @@ namespace CPSC481.FinalProject
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            DemoVideoPage demo = new();
+            LandingScreen demo = new();
             this.Content = demo;
         }
     }
