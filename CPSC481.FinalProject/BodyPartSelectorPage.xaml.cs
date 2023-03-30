eac1004d329c0c72ba713edfcc15fe4f58626ce5
Muhammad Razeen Adil <razeen.adil16@gmail.com>
2023-03-29 20:03:43 -0600

    added some stuff

--- CPSC481.FinalProject/BodyPartSelectorPage.xaml.cs
+++ CPSC481.FinalProject/BodyPartSelectorPage.xaml.cs
@@ -48,21 +48,68 @@ namespace CPSC481.FinalProject
         {
             if (!navigationIsClicked)
             {
+                InfoButton.IsEnabled = true;
+                DemoButton.IsEnabled = true;
+                ProgressButton.IsEnabled = true;
+                RoutineButton.IsEnabled = true;
+
+
                 InfoButton.Visibility = Visibility.Visible;
                 DemoButton.Visibility = Visibility.Visible;
                 ProgressButton.Visibility = Visibility.Visible;
                 RoutineButton.Visibility = Visibility.Visible;
+                ellipseHack.Visibility = Visibility.Visible;
+
                 navigationIsClicked = true;
             }
             else
             {
+                InfoButton.IsEnabled = false;
+                DemoButton.IsEnabled = false;
+                ProgressButton.IsEnabled = false;
+                RoutineButton.IsEnabled = false;
+
                 InfoButton.Visibility = Visibility.Hidden;
                 DemoButton.Visibility = Visibility.Hidden;
                 ProgressButton.Visibility = Visibility.Hidden;
                 RoutineButton.Visibility = Visibility.Hidden;
+                ellipseHack.Visibility = Visibility.Hidden;
+
+
                 navigationIsClicked = false;
             }
 
+        }
+
+        //this is the logout button was to lazy to rename
+        private void InfoButton_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = (MainWindow)Application.Current.MainWindow;
+            mainWindow?.ChangeView(new Welcome());
+        }
+
+        private void ProgressButton_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = (MainWindow)Application.Current.MainWindow;
+            mainWindow?.ChangeView(new ProgressPageWeekly());
+        }
+
+        private void DemoButton_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = (MainWindow)Application.Current.MainWindow;
+            mainWindow?.ChangeView(new BodyPartSelectorPage());
+        }
+
+        private void RoutineButton_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = (MainWindow)Application.Current.MainWindow;
+            mainWindow?.ChangeView(new ViewRoutines());
+        }
+
+        private void Back_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = (MainWindow)Application.Current.MainWindow;
+            mainWindow?.ChangeView(new LandingScreen());
         }
 
         public string Selection
