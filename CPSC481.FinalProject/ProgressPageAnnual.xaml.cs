80bc71cf4cc4e1a7b7812b81a4f7c3cbbe4761c2
SamuelAliSanchez <102827674+SamuelAliSanchez@users.noreply.github.com>
2023-04-05 19:44:06 -0600

    Update ProgressPageAnnual.xaml.cs

--- CPSC481.FinalProject/ProgressPageAnnual.xaml.cs
+++ CPSC481.FinalProject/ProgressPageAnnual.xaml.cs
@@ -1,1 +1,28 @@
-//make an edit
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CPSC481.FinalProject
+{
+    /// <summary>
+    /// Interaction logic for ProgressPageAnnual.xaml
+    /// </summary>
+    public partial class ProgressPageAnnual : Page
+    {
+        public ProgressPageAnnual()
+        {
+            InitializeComponent();
+        }
+    }
+}
