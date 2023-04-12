3643a8ee0a733d31dd3fb0d7357449ec4582873e
a2-b2 <101610797+a2-b2@users.noreply.github.com>
2023-04-11 18:33:24 -0600

    Finished ViewRoutine screen
    - Created UserControl for ViewRoutine screen list of routines buttons.
    - Connected each button to the routine start screen that it should go to.

---
+++ CPSC481.FinalProject/RoutineItem.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CPSC481.FinalProject
+{
+    /// <summary>
+    /// Interaction logic for RoutineItem.xaml
+    /// </summary>
+    public partial class RoutineItem : UserControl
+    {
+        private string routine_name;
+        public string RoutineName
+        {
+            get { return routine_name; }
+            set
+            { 
+                routine_name = value;
+                this.Routine_Name.Text = routine_name;
+            }
+        }
+
+        private string routine_date;
+        public string RoutineDate
+        {
+            get { return routine_date; }
+            set
+            {
+                routine_date = value;
+                this.Routine_Date.Text = routine_date;
+            }
+        }
+
+        public RoutineItem()
+        {
+            InitializeComponent();
+        }
+
+        private void Routine_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = (MainWindow)Application.Current.MainWindow;
+            Global_Data.routine_chosen = routine_name;
+            mainWindow?.ChangeView(new RoutineStartScreen());
+        }
+    }
+}
